a3df7f40698127621feb93817e166727b5fa4e20
Muhammad-Rebaal <mrebaal14@gmail.com>
2024-02-12 21:58:22 +0500

    Added

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,16 @@
+#!/bin/bash
+
+# Echo message indicating the start of the build process
 echo "BUILD START"
+
+# Activate virtual environment
+source /path/to/your/venv/bin/activate
+
+# Install dependencies from requirements.txt
 python3 -m pip install -r requirements.txt
+
+# Collect static files
 python3 manage.py collectstatic --noinput --clear
+
+# Echo message indicating the end of the build process
 echo "BUILD END"
