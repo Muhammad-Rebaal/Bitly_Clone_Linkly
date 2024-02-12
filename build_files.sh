ff787b253d8cfb4a78e53269fa9e5cdf2e188956
Muhammad-Rebaal <mrebaal14@gmail.com>
2024-02-12 22:11:40 +0500

    Added

--- build_files.sh
+++ build_files.sh
@@ -4,7 +4,7 @@
 echo "BUILD START"
 
 # Activate virtual environment
-source /env/Scripts/activate
+source env/Scripts/activate
 
 # Install dependencies from requirements.txt
 python3 -m pip install -r requirements.txt
